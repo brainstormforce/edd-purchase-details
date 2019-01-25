91607da6232000891f6be4feb3b11e8b770f3444
Gaibiso Nadaf <gaibison@bsf.io>
2019-01-25 16:01:53 +0530

    View lincence changes

--- classes/class-edd-pd-frontend.php
+++ classes/class-edd-pd-frontend.php
@@ -161,10 +161,9 @@ if ( ! class_exists( 'EDD_PD_Frontend' ) ) {
 					$customer_details = get_user_by( 'email', $email );
 
 					if ( ! empty( $customer_details ) ) {
-						$payments = edd_get_users_purchases( $customer_details->ID, 50, true, 'any' );
-						if ( $payments ) :
-
-							do_action( 'edd_before_purchase_history', $payments );
+						$payment_ids = edd_get_users_purchases( $customer_details->ID, 50, true, 'any' );
+						if ( $payment_ids ) :
+							do_action( 'edd_before_purchase_history', $payment_ids );
 							?>
 							<div class="entry-content product_details clear">
 								<table id="edd_pd_user_history" class="product_details_table">
@@ -176,25 +175,26 @@ if ( ! class_exists( 'EDD_PD_Frontend' ) ) {
 											<th class="edd_purchase_products"><?php _e( 'Products', 'edd-purchase-details' ); ?></th>
 											<th class="edd_purchase_amount"><?php _e( 'Amount', 'edd-purchase-details' ); ?></th>
 											<th class="edd_purchase_status"><?php _e( 'Status', 'edd-purchase-details' ); ?></th>
-											<th class="edd_purchase_details"><?php _e( 'key', 'edd-purchase-details' ); ?></th>
+											<th class="edd_purchase_details"><?php _e( 'License Keys', 'edd-purchase-details' ); ?></th>
 											<?php do_action( 'access_to_purchase_details_header_after' ); ?>
 										</tr>
 									</thead>
 									<tbody>
 									<?php
-									foreach ( $payments as $payment ) :
-										$payment = new EDD_Payment( $payment->ID );
+									foreach ( $payment_ids as $payment ) :
+										$purchase = new EDD_Payment( $payment->ID );
+
 										?>
 										<tr class="edd_purchase_row">
 											<?php do_action( 'access_to_purchase_details_row_start', $payment->ID, $payment->payment_meta ); ?>
-											<td class="edd_purchase_id">#<?php echo $payment->number; ?></td>
-											<td class="edd_purchase_date"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $payment->date ) ); ?></td>
-											<td class="edd_purchase_products"><?php echo  $payment->cart_details[0]['name']; ?></span>
+											<td class="edd_purchase_id">#<?php echo $purchase->number; ?></td>
+											<td class="edd_purchase_date"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $purchase->date ) ); ?></td>
+											<td class="edd_purchase_products"><?php echo  $purchase->cart_details[0]['name']; ?></span>
 											</td>
-											<td class="edd_purchase_amount"><?php echo edd_currency_filter( edd_format_amount( $payment->total ) ); ?></span>
+											<td class="edd_purchase_amount"><?php echo edd_currency_filter( edd_format_amount( $purchase->total ) ); ?></span>
 											</td>
 											<td class="edd_purchase_details">
-													<?php echo $payment->status_nicename; ?>
+													<?php echo $purchase->status_nicename; ?>
 											</td>
 											<td class="edd_purchase_key">
 											<?php
@@ -202,13 +202,17 @@ if ( ! class_exists( 'EDD_PD_Frontend' ) ) {
 												add_query_arg(
 													array(
 														'action'     => 'view_history',
-														'payment_id' => $payment->ID,
+														'payment_id' => $purchase->ID,
 													)
 												)
 											);
+											if ( edd_is_payment_complete( $payment->ID ) && edd_software_licensing()->get_licenses_of_purchase( $payment->ID ) ) {
+												echo '<a href="' . esc_url( $history ) . '">' . __( 'View Licenses', 'edd-purchase-details' ) . '</a>';
+											} else {
+												echo ' - ';
+											}
+											do_action( 'access_to_purchase_details_row_end', $purchase->ID, $payment->payment_meta );
 											?>
-											<a href='<?php echo esc_url( $history ); ?>'><?php echo $payment->key; ?> </a>
-											<?php do_action( 'access_to_purchase_details_row_end', $payment->ID, $payment->payment_meta ); ?>
 										</td>
 									</tr>
 									<?php endforeach; ?>
@@ -243,7 +247,7 @@ if ( ! class_exists( 'EDD_PD_Frontend' ) ) {
 		 * Display the Product History  .
 		 *
 		 * @since 0.0.1
-		 * @param  Int $payment_id   View history by payment_id
+		 * @param  Int $payment_id   View history by payment_id.
 		 * @return void
 		 */
 		function view_history( $payment_id ) {
