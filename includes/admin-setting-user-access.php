69bf48a8f511439bed910f3dd695cab19b4eef80
Raj <rajkiranb@bsf.io>
2020-02-10 16:11:43 +0530

    completed phpcs of admin-setting-user-access.php file

--- includes/admin-setting-user-access.php
+++ includes/admin-setting-user-access.php
@@ -12,8 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="wrap">
-	<h1><?php _e( 'Access Payment History ', 'edd-purchase-details' ); ?> </h1>
-	<h3><?php _e( 'Manage which user roles can access purchase details information.', 'edd-purchase-details' ); ?></h3>
+	<h1><?php esc_html_e( 'Access Payment History ', 'edd-purchase-details' ); ?> </h1>
+	<h3><?php esc_html_e( 'Manage which user roles can access purchase details information.', 'edd-purchase-details' ); ?></h3>
 	<form action="options.php" method="post" class="edd_pd_setting_form" >
 		<div class="epf_save_setting">
 		<?php
@@ -23,15 +23,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 		foreach ( get_editable_roles() as $role_name => $role_info ) :
 			$name = translate_user_role( $role_name );
 			if ( ! empty( $roles ) ) {
-				$checked = in_array( $role_name, $roles ) ? 'checked' : '';
-			} elseif ( 'administrator' == $role_name ) {
+				$checked = in_array( $role_name, $roles, true ) ? 'checked' : '';
+			} elseif ( 'administrator' === $role_name ) {
 				$checked = 'checked';
 			} else {
 				$checked = '';
 			}
 			?>
 			<fieldset>
-				<input type="checkbox" name="edd_pd_user_access[]" value="<?php echo $role_name; ?>" <?php echo $checked; ?>>
+				<input type="checkbox" name="edd_pd_user_access[]" value="<?php echo esc_attr( $role_name ); ?>" <?php echo esc_attr( $checked ); ?>>
 				<?php echo esc_html( ucwords( str_replace( '_', ' ', $role_name ) ) ); ?>
 			</fieldset>
 			<?php
@@ -45,10 +45,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="clear"></div>
 <hr>
 <div class="epf-user-guides">
-	<h3> <?php _e( 'Getting Started', 'edd-purchase-details' ); ?> </h3>
+	<h3> <?php esc_html_e( 'Getting Started', 'edd-purchase-details' ); ?> </h3>
 	<fieldset>
 		<legend class="screen-reader-text"><span>Note : </span></legend>
-		<p><label for="comment_max_links"> <?php _e( 'Copy this shortcode and paste it into your post or page', 'edd-purchase-details' ); ?> </p>
+		<p><label for="comment_max_links"> <?php esc_html_e( 'Copy this shortcode and paste it into your post or page', 'edd-purchase-details' ); ?> </p>
 		<span class="shortcode"><input type="text" onfocus="this.select();" size ="30" readonly="readonly" value="[access_to_purchase_details]" class="code"></span>
 	</fieldset>
 </div>
