706e69eb41a7895134287b6a65dac5ab3e1f506c
Raj <rajkiranb@bsf.io>
2020-05-04 13:00:02 +0530

    used translation function

--- classes/class-edd-purchase-details-frontend.php
+++ classes/class-edd-purchase-details-frontend.php
@@ -318,7 +318,7 @@ if ( ! class_exists( 'EDD_Purchase_Details_Frontend' ) ) {
 										echo esc_attr( $child_key->expiration );
 									} else if ( 'lifetime' === $child_key->expiration ) {
 										
-										echo 'Lifetime';
+										echo _e( 'Lifetime', 'edd-purchase-details' );
 									} else {
 										echo esc_html( date_i18n( 'F j, Y', $child_key->expiration ) );
 									}
